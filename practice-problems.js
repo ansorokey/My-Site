325d794310bfb5f0e6a8149a5314d71f355a2a33
Anton <ansorokey@gmail.com>
2022-09-26 11:35:42 -0400

    Removed root from image sources

--- practice-problems.js
+++ practice-problems.js
@@ -1,6 +1,6 @@
 $("li").click(function(event){
-    var imgSolution = "/images/problem-solutions/" + $(this).attr("id") + "-solution.png";
-    var imgScenario = "/images/problem-scenarios/" + $(this).attr("id") + "-scenario.png";
+    var imgSolution = "images/problem-solutions/" + $(this).attr("id") + "-solution.png";
+    var imgScenario = "images/problem-scenarios/" + $(this).attr("id") + "-scenario.png";
     $("#scenario").attr("src", imgScenario);
     $("#solution").attr("src", imgSolution);
 });
